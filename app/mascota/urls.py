6f55e6c74f1ee7dfa30725d2aed149586dac7f4a
Juan David <dejuata@hotmail.com>
2017-03-02 02:06:54 +0000

    Vistas basadas en clases - ListView y CreateView

--- app/mascota/urls.py
+++ app/mascota/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
-from app.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete
+from app.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete, \
+    MascotaList, MascotaCreate
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^new$', mascota_view, name='mascota_crear'),
-    url(r'^list$', mascota_list, name='mascota_list'),
+    url(r'^new$', MascotaCreate.as_view(), name='mascota_crear'),
+    url(r'^list$', MascotaList.as_view(), name='mascota_list'),
     url(r'^edit/(?P<id_mascota>\d+)/$', mascota_edit, name='mascota_edit'),
     url(r'^delete/(?P<id_mascota>\d+)/$', mascota_delete, name='mascota_delete'),
 ]
--- app/mascota/views.py
+++ app/mascota/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.views.generic import ListView, CreateView
+from django.core.urlresolvers import reverse_lazy
 from app.mascota.forms import MascotaForm
 from app.mascota.models import Mascota
 
@@ -40,3 +41,13 @@ def mascota_delete(request, id_mascota):
 		mascota.delete()
 		return redirect('mascota:mascota_list')
 	return render(request, 'mascota/mascota_delete.html', {'mascota':mascota})
+	
+class MascotaList(ListView):
+	model = Mascota
+	template_name = 'mascota/mascota_list.html'
+	
+class MascotaCreate(CreateView):
+	model = Mascota
+	form_class = MascotaForm
+	template_name = 'mascota/mascota_form.html'
+	success_url = reverse_lazy('mascota:mascota_list')
