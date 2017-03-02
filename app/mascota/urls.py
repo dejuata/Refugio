de403bebfc79127b1ce7e4d82d95e0229edaf15b
Juan David <dejuata@hotmail.com>
2017-03-02 01:39:39 +0000

    Vistas basadas en funciones - actualizar y eliminar

--- app/mascota/urls.py
+++ app/mascota/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
-from app.mascota.views import index, mascota_view, mascota_list
+from app.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^new$', mascota_view, name='mascota_crear'),
     url(r'^list$', mascota_list, name='mascota_list'),
+    url(r'^edit/(?P<id_mascota>\d+)/$', mascota_edit, name='mascota_edit'),
+    url(r'^delete/(?P<id_mascota>\d+)/$', mascota_delete, name='mascota_delete'),
 ]
