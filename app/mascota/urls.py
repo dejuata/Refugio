5f0a6f57635245f39de1b25131030fd91fbbefd9
Juan David <dejuata@hotmail.com>
2017-03-07 03:38:28 +0000

    Vistas basadas en clases - UpdateView y DeleteView

--- app/mascota/urls.py
+++ app/mascota/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
 from app.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete, \
-    MascotaList, MascotaCreate
+    MascotaList, MascotaCreate, MascotaUpdate, MascotaDelete
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^new$', MascotaCreate.as_view(), name='mascota_crear'),
     url(r'^list$', MascotaList.as_view(), name='mascota_list'),
-    url(r'^edit/(?P<id_mascota>\d+)/$', mascota_edit, name='mascota_edit'),
-    url(r'^delete/(?P<id_mascota>\d+)/$', mascota_delete, name='mascota_delete'),
+    url(r'^edit/(?P<pk>\d+)/$', MascotaUpdate.as_view(), name='mascota_edit'),
+    url(r'^delete/(?P<pk>\d+)/$', MascotaDelete.as_view(), name='mascota_delete'),
 ]
--- app/mascota/views.py
+++ app/mascota/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from app.mascota.forms import MascotaForm
 from app.mascota.models import Mascota
@@ -50,4 +50,15 @@ class MascotaCreate(CreateView):
 	model = Mascota
 	form_class = MascotaForm
 	template_name = 'mascota/mascota_form.html'
+	success_url = reverse_lazy('mascota:mascota_list')
+	
+class MascotaUpdate(UpdateView):
+	model = Mascota
+	form_class = MascotaForm
+	template_name = 'mascota/mascota_form.html'
+	success_url = reverse_lazy('mascota:mascota_list')
+	
+class MascotaDelete(DeleteView):
+	model = Mascota
+	template_name = 'mascota/mascota_delete.html'
 	success_url = reverse_lazy('mascota:mascota_list')
