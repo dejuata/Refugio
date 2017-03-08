3a46cd5e1830f93abd46291776c1133a31935a44
Juan David <dejuata@hotmail.com>
2017-03-08 16:23:13 +0000

    Recuperar contraseñas por correo

--- refugio/settings.py
+++ refugio/settings.py
@@ -129,3 +129,11 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_REDIRECT_URL = reverse_lazy('adopcion:solicitud_list')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'juan.david.pino.reyes@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
--- refugio/urls.py
+++ refugio/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth.views import login, logout_then_login
+from django.contrib.auth.views import login, logout_then_login, password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
 urlpatterns = [
     url(r'^$', login, {'template_name':'index.html'}, name='login' ),
@@ -26,4 +26,17 @@ urlpatterns = [
     url(r'^usuario/', include('app.usuario.urls', namespace='usuario')),
     url(r'^accounts/login/', login, {'template_name':'index.html'}, name='login'),
     url(r'^logout/', logout_then_login, name='logout'),
+    url(r'^reset/password_reset/$', password_reset, 
+        {'template_name':'registration/password_reset_form.html',
+        'email_template_name': 'registration/password_reset_email.html'}, 
+        name='password_reset'), 
+    url(r'^password_reset_done/$', password_reset_done, 
+        {'template_name': 'registration/password_reset_done.html'}, 
+        name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', password_reset_confirm, 
+        {'template_name': 'registration/password_reset_confirm.html'},
+        name='password_reset_confirm'
+        ),
+    url(r'^reset/done', password_reset_complete, {'template_name': 'registration/password_reset_complete.html'},
+        name='password_reset_complete'),
 ]
