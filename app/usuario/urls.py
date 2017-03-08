e74ddcd73534fed48148ec2b819c7c2008193fec
Juan David <dejuata@hotmail.com>
2017-03-08 13:35:23 +0000

    Creacion de registros de usuarios para la app

--- app/usuario/urls.py
+++ app/usuario/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
 
+from app.usuario.views import RegistroUsuario
 
 urlpatterns = [
-    #url(r'^$', index, name='index'),
+    url(r'^registrar/$', RegistroUsuario.as_view(), name='registrar'),
     
 ]
