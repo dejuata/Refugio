66cbc7d9cbf5b41dcd97bfe29668f7c63da97042
Juan David <dejuata@hotmail.com>
2017-03-09 04:42:03 +0000

    xRestframework ModelSerializer

--- app/usuario/urls.py
+++ app/usuario/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 
-from app.usuario.views import RegistroUsuario
+from app.usuario.views import RegistroUsuario, UserAPI
 
 urlpatterns = [
-    url(r'^registrar/$', RegistroUsuario.as_view(), name='registrar'),
-    
+	url(r'^registrar', RegistroUsuario.as_view(), name="registrar"),
+	url(r'^api', UserAPI.as_view(), name="api"),
+
 ]
