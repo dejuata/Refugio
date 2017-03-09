45a7f6c6dca7c9ac11231375a4ed70173f32b22b
Juan David <dejuata@hotmail.com>
2017-03-09 04:23:56 +0000

    Paginación

--- app/mascota/.~c9_invoke_rizmce.py
+++ app/mascota/.~c9_invoke_rizmce.py
@@ -51,7 +51,7 @@ def mascota_delete(request, id_mascota):
 class MascotaList(ListView):
 	model = Mascota
 	template_name = 'mascota/mascota_list.html'
-	paginate_by = 5
+	paginate
 	
 class MascotaCreate(CreateView):
 	model = Mascota
--- app/mascota/urls.py
+++ app/mascota/urls.py
@@ -7,7 +7,7 @@ from app.mascota.views import index, listado, mascota_view, mascota_list, mascot
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^new$', login_required(MascotaCreate.as_view()), name='mascota_crear'),
-    url(r'^list$', login_required(MascotaList.as_view()), name='mascota_list'),
+    url(r'^list', login_required(MascotaList.as_view()), name='mascota_list'),
     url(r'^edit/(?P<pk>\d+)/$', login_required(MascotaUpdate.as_view()), name='mascota_edit'),
     url(r'^delete/(?P<pk>\d+)/$', login_required(MascotaDelete.as_view()), name='mascota_delete'),
     url(r'^listado', listado, name='listado'),
