2899c167d61819ee149d5eb55833035725804963
Juan Pino <dejuata@hotmail.com>
2017-03-12 12:24:27 -0500

    Update settings.py

--- refugio/settings.py
+++ refugio/settings.py
@@ -134,6 +134,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 25
 EMAIL_HOST_USER = 'juan.david.pino.reyes@gmail.com'
-EMAIL_HOST_PASSWORD = 'arbolito1143859996'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
